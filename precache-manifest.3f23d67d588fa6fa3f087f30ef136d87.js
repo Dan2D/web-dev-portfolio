00e1cbeeda7c448c7c9c1243c2a541938c116801
Dan Schultz <travelguy90@gmail.com>
2019-07-24 11:39:10 -0700

    Updates

---
+++ precache-manifest.3f23d67d588fa6fa3f087f30ef136d87.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb0abe48c661876d2cb17e359b18f83",
+    "url": "/web-dev-portfolio/index.html"
+  },
+  {
+    "revision": "9b522a0d57e31633eee3",
+    "url": "/web-dev-portfolio/static/css/main.1d119189.chunk.css"
+  },
+  {
+    "revision": "7c75001aa1baaaaa65e8",
+    "url": "/web-dev-portfolio/static/js/2.cc83ebc0.chunk.js"
+  },
+  {
+    "revision": "9b522a0d57e31633eee3",
+    "url": "/web-dev-portfolio/static/js/main.b319e8b9.chunk.js"
+  },
+  {
+    "revision": "7b3304ee80c765404cee",
+    "url": "/web-dev-portfolio/static/js/runtime~main.95766ac1.js"
+  },
+  {
+    "revision": "9781c375c188995219c03ca0772683e6",
+    "url": "/web-dev-portfolio/static/media/Bootstrap_Logo.9781c375.png"
+  },
+  {
+    "revision": "07270c13412737933c6f3468d97618ad",
+    "url": "/web-dev-portfolio/static/media/CSS3_Logo.07270c13.png"
+  },
+  {
+    "revision": "99bc1b4f379b417e3f7777d066b68cfa",
+    "url": "/web-dev-portfolio/static/media/Ellipse.99bc1b4f.svg"
+  },
+  {
+    "revision": "98ff4822d03ab0f5746653f56441d38e",
+    "url": "/web-dev-portfolio/static/media/Express_logo.98ff4822.png"
+  },
+  {
+    "revision": "90239c2a3e072523b4793e74e211b14b",
+    "url": "/web-dev-portfolio/static/media/Figma_Logo.90239c2a.png"
+  },
+  {
+    "revision": "0992cdbd4a2f352cce496e6d30389fa9",
+    "url": "/web-dev-portfolio/static/media/Git_Logo.0992cdbd.png"
+  },
+  {
+    "revision": "384d1e8c3c8dcfee10ab0b980f8075cd",
+    "url": "/web-dev-portfolio/static/media/Javascript_Logo.384d1e8c.png"
+  },
+  {
+    "revision": "b577a198f80dcb1a100a970a555de06c",
+    "url": "/web-dev-portfolio/static/media/Khula-Bold.b577a198.ttf"
+  },
+  {
+    "revision": "1e35bfe4d8ec1a775eb30f5428274e09",
+    "url": "/web-dev-portfolio/static/media/Khula-Light.1e35bfe4.ttf"
+  },
+  {
+    "revision": "8de2e00caf4243bc899385bfaa78e01a",
+    "url": "/web-dev-portfolio/static/media/MongoDB_Logo.8de2e00c.png"
+  },
+  {
+    "revision": "c0da5cca5bb47bcd789f536be99aa4a5",
+    "url": "/web-dev-portfolio/static/media/Mt.Rainier-night.c0da5cca.jpg"
+  },
+  {
+    "revision": "36700bee9a1831f2691165d291d62d78",
+    "url": "/web-dev-portfolio/static/media/Nodejs_Logo.36700bee.png"
+  },
+  {
+    "revision": "e485b6303071b9192161f40e4ba445fa",
+    "url": "/web-dev-portfolio/static/media/Photoshop_Logo.e485b630.png"
+  },
+  {
+    "revision": "1535aa30ed0372c2daf5138f117ecc79",
+    "url": "/web-dev-portfolio/static/media/React Redux_Logo.1535aa30.png"
+  },
+  {
+    "revision": "5abd169ce9c94afe31962dc40d4f7eb8",
+    "url": "/web-dev-portfolio/static/media/React Router_Logo.5abd169c.png"
+  },
+  {
+    "revision": "10e47b8b3e5449ec77026682926cef33",
+    "url": "/web-dev-portfolio/static/media/React.js_Logo.10e47b8b.png"
+  },
+  {
+    "revision": "cd84e193d6939ab1510eeb93bdc3789b",
+    "url": "/web-dev-portfolio/static/media/Webpack_Logo.cd84e193.png"
+  },
+  {
+    "revision": "b0c5316f56692fee95d77055a61f3b63",
+    "url": "/web-dev-portfolio/static/media/backpack.b0c5316f.png"
+  },
+  {
+    "revision": "11d7dc7b4f9a6c925d729d72a79f0457",
+    "url": "/web-dev-portfolio/static/media/books-desktop.11d7dc7b.png"
+  },
+  {
+    "revision": "85fceff33b5957efe4080d5b606638b9",
+    "url": "/web-dev-portfolio/static/media/jQuery_Logo.85fceff3.png"
+  },
+  {
+    "revision": "623ee20f8498b32744713c38cdf97706",
+    "url": "/web-dev-portfolio/static/media/mystery-desktop.623ee20f.png"
+  },
+  {
+    "revision": "5d9e77d434bb4316d6f8f14becd67f1e",
+    "url": "/web-dev-portfolio/static/media/mystery.5d9e77d4.png"
+  },
+  {
+    "revision": "a64c74cff25694a41f13c4a4659ff9a8",
+    "url": "/web-dev-portfolio/static/media/run-desktop.a64c74cf.png"
+  },
+  {
+    "revision": "639058f09e18c6b0e5bd96a1377d5e2e",
+    "url": "/web-dev-portfolio/static/media/run.639058f0.png"
+  }
+]);
