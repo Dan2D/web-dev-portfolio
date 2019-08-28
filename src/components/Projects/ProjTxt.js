a47d686bf97e61e3d2571d4e3e9693df3f619dbe
Dan Schultz <travelguy90@gmail.com>
2019-08-28 16:42:19 -0700

    updated resume and mobile width fix

--- src/components/Projects/ProjTxt.js
+++ src/components/Projects/ProjTxt.js
@@ -1,14 +1,14 @@
 export const projTxt = {
     mystery: {
-        purpose: ["Create a way to introduce players to their characters, secret information, rules of the game, and guides for a more immersive party."],
+        purpose: ["Murder Mystery Party Launch Site with a focus on introducing players to their characters, understanding rules, and tips/tricks for getting into character."],
         notes: ["Responsive Static Site", "Animated Page Transitions and sub-menu", "Character Modal for password entry which reveals secret info"]
     },
     books: {
-        purpose: ["Create a site to help readers to find new books they’re interested in and explore new titles and authors."],
+        purpose: ["Book exploration site to help readers to find new interesting books and explore titles, genres, and authors."],
         notes: ["API Interface with NYT Best sellers and GoodReads", "Searchable authors and titles", "Genre specific NYT Best Seller lists searchable by date"]
     },
     run: {
-        purpose: ["Provide a way for myself and others to track their running goals and progress towards those goals through individual runs.", 
+        purpose: ["Running Tracker App for setting goals and tracking progress with a unique and fun style.", 
         "username: \"Test\"", "password: \"Test123!!\""  ],
         notes: ["Asynchronous JWT authentication and user password encryption", "CRUD System for user goals and runs", "Visual progress indicator for completion of goals", "Animated Page Transitions"]
     }
